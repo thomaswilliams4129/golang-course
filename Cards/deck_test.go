3e3dbd8b6b77dbe5425e7ba508b90e105f61ddf6
Thomas Williams <thomaswilliams@Thomass-MacBook-Pro.local>
2021-06-06 23:16:18 -0400

    Added to TestNewDeck two test to check the starting card and ending card in a new deck

--- Cards/deck_test.go
+++ Cards/deck_test.go
@@ -4,10 +4,23 @@ import "testing"
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
-	expectedSize := 53
+	expectedSize := 52
 
 	if len(d) != expectedSize {
 		t.Errorf("Expected deck length of %v, but got %v", expectedSize, len(d))
 	}
 
+	expectedStartingCard := "Ace of Spades"
+	actualStartingCard := d[0]
+
+	if actualStartingCard != expectedStartingCard {
+		t.Errorf("Expected first card to be %v but found %v", expectedStartingCard, actualStartingCard)
+	}
+
+	expectedEndingCard := "King of Diamonds"
+	actualEndingCard := d[len(d)-1]
+
+	if actualEndingCard != expectedEndingCard {
+		t.Errorf("Expected end card to be %v but found %v", expectedEndingCard, actualEndingCard)
+	}
 }
